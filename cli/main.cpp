f9de6061d894543aac9a41f43f936eb1cee24d9d
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-10-08 02:35:02 +0200

    Begin to work on it

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,1 +1,16 @@
-int main() {}
+#include <iostream>
+#include <filesystem>
+#include <tclap/CmdLine.h>
+
+int main(const int argc, const char* argv[])
+{
+    TCLAP::CmdLine cmd("Bunch Rename Tool");
+    TCLAP::UnlabeledValueArg<std::string> srcdirArg("srcdir", "Source Directory containing files", true, ".", "string");
+    cmd.add(srcdirArg);
+    cmd.parse(argc, argv);
+
+    const std::string srcdir = srcdirArg.getValue();
+    for (const auto& entry : std::filesystem::directory_iterator(srcdir)) {
+        std::cout << entry.path().filename() << '\n';
+    }
+}
